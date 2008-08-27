84670b868576fc51a016fcb60f0e0616d2267048
ffantato <ffantato@7995dc58-227d-0410-baee-59875a487a5b>
2008-08-27 17:51:22 +0000

    [244539] The plug-in "org.eclipse.tml.service.start" depends on jdt
    git-svn-id: file:///svnroot/tools/org.eclipse.sequoyah@218 7995dc58-227d-0410-baee-59875a487a5b

---
+++ trunk/org.eclipse.sequoyah.device/examples/org.eclipse.sequoyah.device.qemuarm/src/org/eclipse/sequoyah/device/qemuarm/handler/StartServiceHandler.java
@@ -0,0 +1,56 @@
+/********************************************************************************
+ * Copyright (c) 2007 Motorola Inc.
+ * This program and the accompanying materials are made available under the terms
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Initial Contributors:
+ * Fabio Fantato (Motorola)
+ * 
+ * Contributors:
+ * Fabio Rigo (Eldorado) - Bug [244066] - The services are being run at one of the UI threads
+ * Fabio Fantato (Eldorado) - Bug [244539] - The plug-in "org.eclipse.tml.service.start" depends on jdt
+ ********************************************************************************/
+
+package org.eclipse.tml.device.qemuarm.handler;
+
+import java.util.Map;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.tml.device.qemuarm.QEmuARMLauncher;
+import org.eclipse.tml.framework.device.manager.DeviceManager;
+import org.eclipse.tml.framework.device.model.IInstance;
+import org.eclipse.tml.framework.device.model.handler.IServiceHandler;
+import org.eclipse.tml.framework.device.model.handler.ServiceHandler;
+import org.eclipse.tml.service.start.StartServicePlugin;
+import org.eclipse.tml.service.start.StartServiceResources;
+import org.eclipse.tml.service.start.launcher.DeviceLauncherManager;
+import org.eclipse.tml.service.start.launcher.IDeviceLauncher;
+
+public class StartServiceHandler extends ServiceHandler {
+
+	public IStatus runService(IInstance instance, Map<Object, Object> arguments, IProgressMonitor monitor) {
+		StartServicePlugin.logInfo(StartServiceResources.TML_Start_Service+" Over->"+instance.getName());
+		try {
+			IDeviceLauncher launcher = new QEmuARMLauncher(instance);
+			ILaunch launch = DeviceLauncherManager.launch(launcher,instance.getName());
+			instance.setPID(launcher.getPID());
+		} catch (Throwable t) {
+			return Status.CANCEL_STATUS;
+		}
+		return Status.OK_STATUS;
+	}
+
+	public IStatus updatingService(IInstance instance, IProgressMonitor monitor) {
+		StartServicePlugin.logInfo(StartServiceResources.TML_Start_Service_Update+"->"+instance.getName());
+		return Status.OK_STATUS;
+	}
+	
+	@Override
+	public IServiceHandler newInstance() {		
+		return new StartServiceHandler();
+	}
+}
