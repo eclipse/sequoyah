b109e5d89be19bce4bc3817c765685c7d5026a00
ffantato <ffantato@7995dc58-227d-0410-baee-59875a487a5b>
2008-12-16 23:27:20 +0000

    [257296] Build script
    git-svn-id: file:///svnroot/tools/org.eclipse.sequoyah@707 7995dc58-227d-0410-baee-59875a487a5b

---
+++ trunk/org.eclipse.sequoyah.device/features/org.eclipse.sequoyah.site.feature/drops/buildNotes.php
@@ -0,0 +1,94 @@
+<!doctype html public "-//w3c//dtd html 4.0 transitional//en">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="stylesheet" href="http://www.eclipse.org/default_style.css" type="text/css">
+<title>Build Notes for TmL 0.1</title>
+</head>
+
+<body>
+<table border="0" cellspacing="5" cellpadding="2" width="100%">
+	<tr>
+		<td align="LEFT" width="80%">
+		<p><b><font class=indextop>Build Notes for TmL 0.3M4</font></b><br>
+		Oct 10.2008 </p>
+		</td>
+	</tr>
+</table>
+<table border="0" cellspacing="5" cellpadding="2" width="100%">
+	<tr>
+		<td align="LEFT" valign="TOP" colspan="3" bgcolor="#0080C0"><b>
+		<font face="Arial,Helvetica" color="#FFFFFF">New and Noteworthy</font></b></td>
+	</tr>
+</table>
+<table><tbody><tr><td>
+<ul>
+<li>There is a new update site:
+    <a href="http://download.eclipse.org/dsdp/tml/updates/0.3M4">
+    http://download.eclipse.org/dsdp/tm/signedUpdates</a>.<br/>
+    We will continue with more testing and migrate the main update site to
+    use signed jars as well as soon as possible.</li>
+<li>This is the first release of TmL, please help us to keep it growing up. Report bugs and send us your suggestions.</li>
+<li>Use
+  <a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&classification=DSDP&product=TmL&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&resolution=FIXED&resolution=WONTFIX&resolution=INVALID&resolution=WORKSFORME&chfieldfrom=2006-11-03&chfieldto=2008-10-10&chfield=resolution&cmdtype=doit">
+  this query</a> to show the list of bugs fixed.</li>
+<li>For details on checkins, see the
+  <a href="http://download.eclipse.org/dsdp/tml/downloads/drops/R-0.3M4/tml-log.cvs">
+  TmL CVS changelog</a>.</li>
+<li>For other questions, please check the
+  <a href="http://wiki.eclipse.org/index.php/DSDP/TML/TML_FAQ">TmL FAQ</a></li>
+</ul>
+</td></tr></tbody></table>
+
+<table border="0" cellspacing="5" cellpadding="2" width="100%">
+	<tr>
+		<td align="LEFT" valign="TOP" colspan="3" bgcolor="#0080C0"><b>
+		<font face="Arial,Helvetica" color="#FFFFFF">Getting Started</font></b></td>
+	</tr>
+</table>
+<table><tbody><tr><td>
+<p>The TmL Documentation now has a
+<a href="http://wiki.eclipse.org/DSDP/TML">
+wiki</a> that contains some guides through installation, first steps,
+connection setup and important tasks.</p>
+</td></tr></tbody></table>
+
+<table border="0" cellspacing="5" cellpadding="2" width="100%">
+	<tr>
+		<td align="LEFT" valign="TOP" colspan="3" bgcolor="#0080C0"><b>
+		<font face="Arial,Helvetica" color="#FFFFFF">API Status</font></b></td>
+	</tr>
+</table>
+<table><tbody><tr><td>
+<p>As per the TmL
+<a href="http://www.eclipse.org/dsdp/tml/development/plan.php">plan</a>,
+we will have small API changes for 0.2 release in December,2008.</p>
+<p>All such API changes will be voted on
+by committers on the <a href="http://dev.eclipse.org/mailman/listinfo/dsdp-tml-dev">
+dsdp-tml-dev</a> developer mailing list, and documented in a migration guide
+for future releases.</p>
+If you want to start programming against TmL APIs now, best let us know
+about your endeavours and keep yourself up-to-date. Stay in contact with
+the <a href="http://dev.eclipse.org/mailman/listinfo/dsdp-tml-dev">
+dsdp-tml-dev</a> developer mailing list, and give feedback to make the
+APIs better.
+</td></tr></tbody></table>
+
+
+<table border="0" cellspacing="5" cellpadding="2" width="100%">
+	<tr>
+		<td align="LEFT" valign="TOP" colspan="3" bgcolor="#0080C0"><b>
+		<font face="Arial,Helvetica" color="#FFFFFF">Bugzilla reports</font></b></td>
+	</tr>
+</table>
+<table><tbody><tr><td>
+<p>Click
+<a href="https://bugs.eclipse.org/bugs/report.cgi?x_axis_field=bug_severity&y_axis_field=op_sys&z_axis_field=&query_format=report-table&classification=DSDP&product=TmL&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&format=table&action=wrap">here</a>
+for a complete up-to-date bugzilla status report, or
+<a href="https://bugs.eclipse.org/bugs/report.cgi?x_axis_field=bug_severity&y_axis_field=op_sys&z_axis_field=&query_format=report-table&classification=DSDP&product=TmL&component=RSE&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&target_milestone=0.1&format=table&action=wrap">here</a>
+for a report on bugs fixed so far.
+</p>
+</td></tr></tbody></table>
+
+</body>
+</html>
---
+++ trunk/org.eclipse.sequoyah.device/features/org.eclipse.sequoyah.site.feature/drops/index.php
@@ -0,0 +1,173 @@
+<!doctype html public "-//w3c//dtd html 4.0 transitional//en">
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="stylesheet" href="http://www.eclipse.org/default_style.css" type="text/css">
+<title>DSDP-TmL Downloads</title>
+</head>
+
+<body>
+<table border="0" cellspacing="5" cellpadding="2" width="100%">
+	<tr>
+		<td align="LEFT" width="80%">
+		<p><b><font class=indextop>TmL Release Build: 0.3M4</font></b><br>
+		Dec 17, 2008 </p>
+		<p>These downloads are provided under the
+		<a href="http://www.eclipse.org/legal/epl/notice.php">Eclipse.org Software
+		User Agreement</a>.</p>
+
+		<p><font size="+1"><strong>
+		  <a href="buildNotes.php">New and Noteworthy / Build Notes</a>
+		</strong></font></p>
+
+		To download a update site archive, click <a href="http://www.eclipse.org/downloads/download.php?file=/dsdp/tml/downloads/drops/R-0.3M4/TmL-R-0.3M4-updatesite.zip">here</a>
+		<!--To view the map file entries for this build, click
+		<a href="directory.txt">here</a>.<br>
+		To view the compile logs for this build, click
+		<a href="compilelog.txt">here</a>.</p>-->
+		</td>
+	</tr>
+</table>
+<table border="0" cellspacing="5" cellpadding="2" width="100%">
+	<tr>
+		<td align="LEFT" valign="TOP" colspan="3" bgcolor="#0080C0"><b>
+		<font face="Arial,Helvetica" color="#FFFFFF">Requirements</font></b></td>
+	</tr>
+</table>
+<table>
+  <tbody>
+    <tr><td><b>TmL requires Eclipse-3.5M4 Galileo
+       (<a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.5/index.php">download page</a>)
+       or later, e.g.
+       <a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.5.1/index.php">3.4.1</a>.
+    </b></td></tr>
+    <tr><td>Earlier versions will <b>not</b> work!</td></tr>
+  </tbody>
+</table>
+</table>
+
+<table border="0" cellspacing="5" cellpadding="2" width="100%">
+	<tr><td colspan="2">&nbsp;</td></tr>
+</table>
+<table border="0" cellspacing="5" cellpadding="2" width="100%">
+	<tr>
+		<td align="LEFT" valign="TOP" colspan="3" bgcolor="#0080C0"><b>
+		<font face="Arial,Helvetica" color="#FFFFFF">SDK (includes runtime, samples)</font></b></td>
+	</tr>
+</table>
+<table border="0" cellspacing="2" cellpadding="0" width="100%">
+	<tr>
+		<td align="RIGHT" valign="TOP" width="7%">
+		<div align="left">
+			<b>Status</b></div>
+		</td>
+		<td width="30%"><b>Platform</b></td>
+		<td width="40%"><b>Download</b></td>
+		<td width="30%"><b>Notes</b></td>
+		<tr><td><div align=left><img src = "OK.gif" width=19 height=23></div></td>
+		<td>All</td>
+		<td><a href="http://www.eclipse.org/downloads/download.php?file=/dsdp/tml/downloads/drops/R-0.3M4/TmL-SDK-R-0.3M4.zip">TmL-SDK-R-0.3M4.zip</a></td>
+        <td>without sources files</td>
+		<tr><td><div align=left><img src = "OK.gif" width=19 height=23></div></td>
+		<td>All</td>
+		<td><a href="http://www.eclipse.org/downloads/download.php?file=/dsdp/tml/downloads/drops/R-0.3M4/TmL-SDK-R-0.3M4-src.zip">TmL-SDK-R-0.3M4-src.zip</a></td>
+		<td>contain all sources</td>
+</tr>
+</table>
+
+<table border="0" cellspacing="5" cellpadding="2" width="100%">
+	<tr>
+		<td align="LEFT" valign="TOP" colspan="3" bgcolor="#0080C0"><b>
+		<font face="Arial,Helvetica" color="#FFFFFF">Runtime</font></b></td>
+	</tr>
+</table>
+<table border="0" cellspacing="2" cellpadding="0" width="100%">
+	<tr>
+		<td align="RIGHT" valign="TOP" width="7%">
+		<div align="left">
+			<b>Status</b></div>
+		</td>
+		<td width="30%"><b>Platform</b></td>
+		<td width="40%"><b>Download</b></td>
+		<td width="30%"><b>Notes</b></td>
+		<tr><td><div align=left><img src = "OK.gif" width=19 height=23></div></td>
+		<td>All</td>
+		<td><a href="http://www.eclipse.org/downloads/download.php?file=/dsdp/tml/downloads/drops/R-R-0.3M4/TmL-Runtime-R-0.3M4.zip">TmL-Runtime-R-0.3M4.zip</a></td>
+		<td>all components</td>
+</tr>
+
+</table>
+
+
+<table border="0" cellspacing="5" cellpadding="2" width="100%">
+	<tr>
+		<td align="LEFT" valign="TOP" colspan="3" bgcolor="#0080C0"><b>
+		<font face="Arial,Helvetica" color="#FFFFFF">Standalone components</font></b></td>
+	</tr>
+</table>
+<table border="0" cellspacing="2" cellpadding="0" width="100%">
+	<tr>
+		<td align="RIGHT" valign="TOP" width="7%">
+		<div align="left">
+			<b>Status</b></div>
+		</td>
+		<td width="20%"><b>Platform</b></td>
+		<td width="30%"><b>Download</b></td>
+		<td width="30%"><b>Component</b></td>
+		<td width="20%"><b>Pre-req</b></td>
+	    <tr><td><div align=left><img src = "OK.gif" width=19 height=23></div></td>
+		<td>All</td>
+		<td><a href="http://www.eclipse.org/downloads/download.php?file=/dsdp/tml/downloads/drops/R-0.3M4/TmL-Device-R-0.3M4.zip">TmL-Common-R-0.3M4.zip</a></td>
+		<td>Common package</td>
+		<td>none</td>
+		<tr><td><div align=left><img src = "OK.gif" width=19 height=23></div></td>
+		<td>All</td>
+		<td><a href="http://www.eclipse.org/downloads/download.php?file=/dsdp/tml/downloads/drops/R-0.3M4/TmL-Device-R-0.3M4.zip">TmL-Device-R-0.3M4.zip</a></td>
+		<td>Device Framework</td>
+		<td>Common Package</td>
+		<tr><td><div align=left><img src = "OK.gif" width=19 height=23></div></td>
+		<td>All</td>
+		<td><a href="http://www.eclipse.org/downloads/download.php?file=/dsdp/tml/downloads/drops/R-0.3M4/TmL-VNC-R-0.3M4.zip">TmL-VNC-R-0.3M4.zip</a></td>
+		<td>VNC Viewer</td>
+		<td>Protocol mechanism</td>
+		<tr><td><div align=left><img src = "OK.gif" width=19 height=23></div></td>
+		<td>All</td>
+		<td><a href="http://www.eclipse.org/downloads/download.php?file=/dsdp/tml/downloads/drops/R-0.3M4/TmL-Protocol-R-0.3M4.zip">TmL-Protocol-R-0.3M4.zip</a></td>
+		<td>Protocol mechanism</td>
+		<td>none</td>
+
+</tr>
+
+</table>
+
+<table border="0" cellspacing="5" cellpadding="2" width="100%">
+	<tr>
+		<td align="LEFT" valign="TOP" colspan="3" bgcolor="#808080"><b>
+		<font face="Arial,Helvetica" color="#FFFFFF">Examples (includes source)</font></b>
+		</td>
+	</tr>
+</table>
+<table border="0" cellspacing="2" cellpadding="0" width="100%">
+	<tr>
+		<td align="RIGHT" valign="TOP" width="7%">
+		<div align="left">
+			<b>Status</b></div>
+		</td>
+		<td width="20%"><b>Platform</b></td>
+		<td width="30%"><b>Download</b></td>
+		<td width="30%"><b>Component</b></td>
+		<td width="20%"><b>Pre-req</b></td>
+	    <tr><td><div align=left><img src = "OK.gif" width=19 height=23></div></td>
+		<td>All</td>
+		<td><a href="http://www.eclipse.org/downloads/download.php?file=/dsdp/tml/downloads/drops/R-0.3M4/TmL-Examples-R-0.3M4.zip">TmL-Examples-R-0.3M4.zip</a></td>
+		<td>All examples</td>
+		<td>Runtime</td>
+</tr>
+
+</table>
+
+
+<p>&nbsp;</p>
+</body>
+</html>
