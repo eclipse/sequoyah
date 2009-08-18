17128049eacc63baa25e8524a52c5f1f218097bb
dmadruga <dmadruga@7995dc58-227d-0410-baee-59875a487a5b>
2009-08-18 15:01:46 +0000

    [286387][investigate]  -  [fiximprove] [Sign]: Refactor MTJ in order to separate signing common features
    git-svn-id: file:///svnroot/tools/org.eclipse.sequoyah@1870 7995dc58-227d-0410-baee-59875a487a5b

--- branches/pulsar/plugins/org.eclipse.mtj.tfm.sign.core/src/org/eclipse/mtj/tfm/sign/core/SignCore.java
+++ branches/pulsar/plugins/org.eclipse.mtj.tfm.sign.core/src/org/eclipse/mtj/tfm/sign/core/SignCore.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2009 Motorola.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Diego Sandin (Motorola) - Initial implementation
+ */
 package org.eclipse.mtj.tfm.sign.core;
 
 import org.eclipse.core.runtime.Plugin;
