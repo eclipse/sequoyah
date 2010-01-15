7ee754f7acc07d67351a6a7afc799bb4bed45381
dpastore <dpastore@7995dc58-227d-0410-baee-59875a487a5b>
2010-01-15 17:37:33 +0000

    Refactoring TmL to Sequoyah
    git-svn-id: file:///svnroot/tools/org.eclipse.sequoyah@1679 7995dc58-227d-0410-baee-59875a487a5b

---
+++ branches/Refactoring/org.eclipse.sequoyah.localization/plugins/org.eclipse.sequoyah.localization.tools/src/org/eclipse/sequoyah/localization/tools/extensions/providers/TranslatorProvider.java
@@ -0,0 +1,39 @@
+/********************************************************************************
+ * Copyright (c) 2009 Motorola Inc.
+ * All rights reserved. This program and the accompanying materials are made available under the terms
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Initial Contributors:
+ * Vinicius Hernandes (Motorola)
+ * 
+ * Contributors:
+ * name (company) - description.
+ ********************************************************************************/
+package org.eclipse.sequoyah.localization.tools.extensions.providers;
+
+import java.util.List;
+
+import org.eclipse.sequoyah.localization.tools.extensions.classes.ITranslator;
+
+/**
+ *
+ */
+public class TranslatorProvider {
+
+	/**
+	 * @return
+	 */
+	public List<ITranslator> getTranslators() {
+		return null;
+	}
+
+	/**
+	 * @param name
+	 * @return
+	 */
+	public ITranslator getTranslatorByName(String name) {
+		return null;
+	}
+
+}
