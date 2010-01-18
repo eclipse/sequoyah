ca79ec3a683631488e78b00a2aa010382d05d051
dpastore <dpastore@7995dc58-227d-0410-baee-59875a487a5b>
2010-01-18 20:05:26 +0000

    Merging changes from branch Refactoring.
    git-svn-id: file:///svnroot/tools/org.eclipse.sequoyah@1707 7995dc58-227d-0410-baee-59875a487a5b

---
+++ trunk/org.eclipse.sequoyah.device/plugins/org.eclipse.sequoyah.device.service.start/src/org/eclipse/sequoyah/device/service/start/launcher/DefaultConnection.java
@@ -0,0 +1,108 @@
+/********************************************************************************
+ * Copyright (c) 2007 Motorola Inc. All rights reserved.
+ * This program and the accompanying materials are made available under the terms
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Initial Contributor:
+ * Fabio Fantato (Motorola)
+ *
+ * Contributors:
+ * {Name} (company) - description of contribution.
+ ********************************************************************************/
+
+package org.eclipse.sequoyah.device.service.start.launcher;
+
+import org.eclipse.sequoyah.device.framework.model.IConnection;
+
+
+/**
+ * Define a default configuration for one connection. This class should be
+ * replaced if the emulator uses a different way to process the properties data.
+ * 
+ * @author Fabio Fantato
+ */
+public class DefaultConnection implements IConnection {
+	private String display;
+	private String host;
+	private int port;
+	
+	public DefaultConnection(String name) {
+		super();
+		this.display = IConnection. DEFAULT_DISPLAY;
+		this.host =  name;
+		this.port = IConnection.DEFAULT_PORT;
+	}
+
+	public DefaultConnection() {
+		super();
+		this.display = IConnection. DEFAULT_DISPLAY;
+		this.host =  IConnection.DEFAULT_HOST;
+		this.port = IConnection.DEFAULT_PORT;
+	}
+
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.tml.emulator.model.IEmulatorConnection#getDisplay()
+	 */
+	public String getDisplay() {
+		return display;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.tml.emulator.model.IEmulatorConnection#setDisplay(java.lang.String)
+	 */
+	public void setDisplay(String display) {
+		this.display = display;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.tml.emulator.model.IEmulatorConnection#getHost()
+	 */
+	public String getHost() {
+		return host;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.tml.emulator.model.IEmulatorConnection#setHost(java.lang.String)
+	 */
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.tml.emulator.model.IEmulatorConnection#getPort()
+	 */
+	public int getPort() {
+		return port;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.tml.emulator.model.IEmulatorConnection#setPort(int)
+	 */
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.tml.emulator.model.IEmulatorConnection#getStringHost()
+	 */
+	public String getStringHost() {
+		return host + display;
+	}
+
+}
