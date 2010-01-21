8fdf88efe0a8c94c5f5df30835eef1e6db6e6922
dpastore <dpastore@7995dc58-227d-0410-baee-59875a487a5b>
2010-01-21 11:40:49 +0000

    Branch for debugging
    git-svn-id: file:///svnroot/tools/org.eclipse.sequoyah@1742 7995dc58-227d-0410-baee-59875a487a5b

---
+++ branches/Debug_Alfa/org.eclipse.sequoyah.vnc/plugins/org.eclipse.sequoyah.vnc.protocol/src/org/eclipse/sequoyah/vnc/protocol/lib/msgdef/databeans/IMsgDataBean.java
@@ -0,0 +1,28 @@
+/********************************************************************************
+ * This program and the accompanying materials are made available under the terms
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Initial Contributor:
+ * Fabio Rigo
+ *
+ * Contributors:
+ * {Name} (company) - description of contribution.
+ ********************************************************************************/
+package org.eclipse.sequoyah.vnc.protocol.lib.msgdef.databeans;
+
+/**
+ * DESCRIPTION: This interface is implemented by all message data definition 
+ * beans. It does not contain any methods, because it is meant for identity 
+ * purposes only. 
+ * <br>
+ *
+ * RESPONSIBILITY: Provide common identity to all message data definition beans.<br>
+ *
+ * COLABORATORS: None.<br>
+ *
+ * USAGE: This class is not meant to be implemented by clients.
+ */
+public interface IMsgDataBean {
+
+}
---
+++ branches/Debug_Alfa/org.eclipse.sequoyah.vnc/plugins/org.eclipse.sequoyah.vnc.vncviewer/src/org/eclipse/sequoyah/vnc/vncviewer/network/VNCProtocolExceptionHandler.java
@@ -0,0 +1,57 @@
+/********************************************************************************
+ * Copyright (c) 2008 Motorola Inc and Others. All rights reserved.
+ * This program and the accompanying materials are made available under the terms
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Initial Contributor:
+ * Fabio Rigo (Eldorado Research Institute) 
+ * [246212] - Enhance encapsulation of protocol implementer
+ *
+ * Contributors:
+ * Fabio Rigo (Eldorado Research Institute) - Bug [262632] - Avoid providing raw streams to the user in the protocol framework
+ *******************************************************************************/
+
+package org.eclipse.sequoyah.vnc.vncviewer.network;
+
+import java.io.IOException;
+
+import org.eclipse.sequoyah.vnc.protocol.lib.IProtocolExceptionHandler;
+import org.eclipse.sequoyah.vnc.protocol.lib.ProtocolHandle;
+import org.eclipse.sequoyah.vnc.protocol.lib.exceptions.InvalidDefinitionException;
+import org.eclipse.sequoyah.vnc.protocol.lib.exceptions.InvalidInputStreamDataException;
+import org.eclipse.sequoyah.vnc.protocol.lib.exceptions.InvalidMessageException;
+import org.eclipse.sequoyah.vnc.protocol.lib.exceptions.MessageHandleException;
+import org.eclipse.sequoyah.vnc.protocol.lib.exceptions.ProtocolHandshakeException;
+import org.eclipse.sequoyah.vnc.protocol.lib.exceptions.ProtocolRawHandlingException;
+
+public class VNCProtocolExceptionHandler implements IProtocolExceptionHandler {
+
+	public void handleIOException(ProtocolHandle handle, IOException e) {
+		//VNCProtocolRegistry.getInstance().unregister(handle);
+	}
+
+	public void handleInvalidDefinitionException(ProtocolHandle handle,
+			InvalidDefinitionException e) {
+	}
+
+	public void handleInvalidInputStreamDataException(ProtocolHandle handle,
+			InvalidInputStreamDataException e) {
+	}
+
+	public void handleInvalidMessageException(ProtocolHandle handle,
+			InvalidMessageException e) {
+	}
+
+	public void handleMessageHandleException(ProtocolHandle handle,
+			MessageHandleException e) {
+	}
+
+	public void handleProtocolHandshakeException(ProtocolHandle handle,
+			ProtocolHandshakeException e) {
+	}
+
+	public void handleProtocolRawHandlingException(ProtocolHandle handle,
+			ProtocolRawHandlingException e) {
+	}
+}
