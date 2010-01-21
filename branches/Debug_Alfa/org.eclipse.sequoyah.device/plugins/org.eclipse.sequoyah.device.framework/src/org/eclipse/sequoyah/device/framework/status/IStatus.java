679515f3588713c3ded14a7e7c08c71c3e636266
dpastore <dpastore@7995dc58-227d-0410-baee-59875a487a5b>
2010-01-21 11:40:38 +0000

    Branch for debugging
    git-svn-id: file:///svnroot/tools/org.eclipse.sequoyah@1741 7995dc58-227d-0410-baee-59875a487a5b

---
+++ branches/Debug_Alfa/org.eclipse.sequoyah.device/plugins/org.eclipse.sequoyah.device.framework/src/org/eclipse/sequoyah/device/framework/status/IStatus.java
@@ -0,0 +1,67 @@
+/********************************************************************************
+ * Copyright (c) 2007 Motorola Inc.
+ * This program and the accompanying materials are made available under the terms
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Initial Contributors:
+ * Fabio Fantato (Motorola)
+ * 
+ * Contributors:
+ * Fabio Rigo (Eldorado) - [245111] Disable the "Delete" option in popup if the instance is not prepared for deletion
+ * Daniel Barboza Franco (Eldorado Research Institute) - Bug [246082] - Complement bug #245111 by allowing disable of "Properties" option as well
+ ********************************************************************************/
+
+package org.eclipse.sequoyah.device.framework.status;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+
+/**
+ * @author wfr004
+ *
+ */
+public interface IStatus {
+    
+	/**
+	 * @return
+	 */
+	public String getId(); 	
+	public void setId(String id); 	
+	
+	/**
+	 * @return
+	 */
+	public String getName();
+	public void setName(String name);
+	
+	/**
+	 * @return
+	 */
+	public ImageDescriptor getImage();
+	public void setImage(ImageDescriptor image);
+	
+	/**
+	 * @return
+	 */
+	public String toString();
+	
+	/**
+	 * @return
+	 */
+	public Object getParent();	
+	public void setParent(Object instance);
+	/**
+     * @return
+     */
+	public boolean canDeleteInstance();
+	public void setCanDeleteInstance(boolean canDeleteInstance);
+
+	public boolean canEditProperties();
+	public void setCanEditProperties(boolean canEditProperties);
+
+	
+	/**
+	 * @return
+	 */
+	public Object clone();
+}
