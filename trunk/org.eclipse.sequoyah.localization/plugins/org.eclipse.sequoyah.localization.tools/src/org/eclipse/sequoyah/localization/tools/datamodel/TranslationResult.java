ed7fb87647ea09f07efb8090fcb1e09b855d7b06
dpastore <dpastore@7995dc58-227d-0410-baee-59875a487a5b>
2010-01-21 16:54:36 +0000

    Merging changes from branch Refactoring.
    git-svn-id: file:///svnroot/tools/org.eclipse.sequoyah@1753 7995dc58-227d-0410-baee-59875a487a5b

---
+++ trunk/org.eclipse.sequoyah.localization/plugins/org.eclipse.sequoyah.localization.tools/src/org/eclipse/sequoyah/localization/tools/datamodel/TranslationResult.java
@@ -0,0 +1,151 @@
+/********************************************************************************
+ * Copyright (c) 2009 Motorola Inc.
+ * All rights reserved. This program and the accompanying materials are made available under the terms
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Initial Contributors:
+ * Vinicius Hernandes (Motorola)
+ * 
+ * Contributors:
+ * name (company) - description.
+ ********************************************************************************/
+package org.eclipse.sequoyah.localization.tools.datamodel;
+
+import java.util.Date;
+
+import org.eclipse.sequoyah.localization.tools.extensions.classes.ITranslator;
+
+/**
+ *
+ */
+public class TranslationResult {
+
+	private String word;
+
+	private ITranslator translator;
+
+	private String translatedWord;
+
+	private String originalLanguage;
+
+	private String translatedLanguage;
+
+	private Date date;
+
+	private boolean succesful;
+
+	
+	
+	public TranslationResult(String word, ITranslator translator,
+			String translatedWord, String originalLanguage,
+			String translatedLanguage, Date date, boolean succesful) {
+		super();
+		this.word = word;
+		this.translator = translator;
+		this.translatedWord = translatedWord;
+		this.originalLanguage = originalLanguage;
+		this.translatedLanguage = translatedLanguage;
+		this.date = date;
+		this.succesful = succesful;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getWord() {
+		return word;
+	}
+
+	/**
+	 * @param word
+	 */
+	public void setWord(String word) {
+		this.word = word;
+	}
+
+	/**
+	 * @return
+	 */
+	public ITranslator getTranslator() {
+		return translator;
+	}
+
+	/**
+	 * @param translator
+	 */
+	public void setTranslator(ITranslator translator) {
+		this.translator = translator;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getTranslatedWord() {
+		return translatedWord;
+	}
+
+	/**
+	 * @param translatedWord
+	 */
+	public void setTranslatedWord(String translatedWord) {
+		this.translatedWord = translatedWord;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getOriginalLanguage() {
+		return originalLanguage;
+	}
+
+	/**
+	 * @param originalLanguage
+	 */
+	public void setOriginalLanguage(String originalLanguage) {
+		this.originalLanguage = originalLanguage;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getTranslatedLanguage() {
+		return translatedLanguage;
+	}
+
+	/**
+	 * @param translatedLanguage
+	 */
+	public void setTranslatedLanguage(String translatedLanguage) {
+		this.translatedLanguage = translatedLanguage;
+	}
+
+	/**
+	 * @return
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	 * @param date
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	/**
+	 * @return
+	 */
+	public boolean isSuccesful() {
+		return succesful;
+	}
+
+	/**
+	 * @param succesful
+	 */
+	public void setSuccesful(boolean succesful) {
+		this.succesful = succesful;
+	}
+
+}
---
+++ trunk/org.eclipse.sequoyah.localization/plugins/org.eclipse.sequoyah.localization.tools/src/org/eclipse/sequoyah/localization/tools/persistence/ProjectPersistenceManager.java
@@ -0,0 +1,124 @@
+/********************************************************************************
+ * Copyright (c) 2009 Motorola Inc.
+ * All rights reserved. This program and the accompanying materials are made available under the terms
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Initial Contributors:
+ * Vinicius Hernandes (Motorola)
+ * 
+ * Contributors:
+ * name (company) - description.
+ ********************************************************************************/
+package org.eclipse.sequoyah.localization.tools.persistence;
+
+import org.eclipse.sequoyah.localization.tools.datamodel.LocalizationFile;
+import org.eclipse.sequoyah.localization.tools.datamodel.LocalizationProject;
+
+/**
+ * 
+ */
+public class ProjectPersistenceManager {
+
+	private LocalizationProject project;
+
+	public ProjectPersistenceManager(LocalizationProject project) {
+		this.project = project;
+	}
+
+	private boolean dataLoaded;
+
+	/**
+	 * 
+	 */
+	public void saveData() {
+
+	}
+
+	/**
+	 * 
+	 */
+	public void loadData() {
+
+	}
+
+	/**
+	 * @param file
+	 */
+	public void loadDataForFile(LocalizationFile file) {
+
+	}
+
+	/**
+	 * @return
+	 */
+	public boolean hasData() {
+		return false;
+	}
+
+	/**
+	 * @return
+	 */
+	public boolean hasMetadata() {
+		return false;
+	}
+
+	/**
+	 * @return
+	 */
+	public boolean hasExtraInfo() {
+		return false;
+	}
+
+	/**
+	 * 
+	 */
+	public void clearAllData() {
+
+	}
+
+	/**
+	 * @param file
+	 * @return
+	 */
+	public LocalizationFile clearDataForFile(LocalizationFile file) {
+		return null;
+	}
+
+	/**
+	 * @param _class
+	 */
+	public void clearClassReferences(Class _class) {
+
+	}
+
+	/**
+	 * @param file
+	 */
+	public void removeOrphanKeysForFile(LocalizationFile file) {
+
+	}
+
+	/**
+	 * 
+	 */
+	public void removeAllOrphanKeys() {
+
+	}
+
+	/**
+	 * @return
+	 */
+	public boolean isAllDataLoaded() {
+		return false;
+	}
+
+	/**
+	 * @param file
+	 * @return
+	 */
+	public boolean isFileDataLoaded(LocalizationFile file) {
+		return false;
+	}
+
+}
