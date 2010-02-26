49066c57391ed3798bb3dbffe53c4c152f619a05
dpastore <dpastore@7995dc58-227d-0410-baee-59875a487a5b>
2010-02-26 13:05:42 +0000

    Branch for debugging Helios launcher issue.
    git-svn-id: file:///svnroot/tools/org.eclipse.sequoyah@1915 7995dc58-227d-0410-baee-59875a487a5b

---
+++ branches/debug_helios_launcher/org.eclipse.sequoyah.localization/plugins/org.eclipse.sequoyah.localization.stringeditor/src/org/eclipse/sequoyah/localization/stringeditor/editor/operations/AddKeyOperation.java
@@ -0,0 +1,78 @@
+/********************************************************************************
+ * Copyright (c) 2009 Motorola Inc.
+ * All rights reserved. This program and the accompanying materials are made available under the terms
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Initial Contributors:
+ * Marcelo Marzola Bossoni (Eldorado)
+ * 
+ * Contributors:
+ * name (company) - description.
+ ********************************************************************************/
+package org.eclipse.sequoyah.localization.stringeditor.editor.operations;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.sequoyah.localization.stringeditor.datatype.RowInfo;
+import org.eclipse.sequoyah.localization.stringeditor.editor.StringEditorPart;
+
+/**
+ * The operation of adding a new key (row) to the editor.
+ */
+public class AddKeyOperation extends EditorOperation {
+
+	private final RowInfo row;
+
+	public AddKeyOperation(String label, StringEditorPart editor, RowInfo row) {
+		super(label, editor);
+		this.row = row;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.core.commands.operations.AbstractOperation#execute(org.eclipse
+	 * .core.runtime.IProgressMonitor, org.eclipse.core.runtime.IAdaptable)
+	 */
+	@Override
+	public IStatus execute(IProgressMonitor monitor, IAdaptable info)
+			throws ExecutionException {
+		return redo(monitor, info);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.core.commands.operations.AbstractOperation#redo(org.eclipse
+	 * .core.runtime.IProgressMonitor, org.eclipse.core.runtime.IAdaptable)
+	 */
+	@Override
+	public IStatus redo(IProgressMonitor monitor, IAdaptable info)
+			throws ExecutionException {
+		getEditor().addRow(row);
+		getEditor().getEditorViewer().refresh();
+		return Status.OK_STATUS;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.core.commands.operations.AbstractOperation#undo(org.eclipse
+	 * .core.runtime.IProgressMonitor, org.eclipse.core.runtime.IAdaptable)
+	 */
+	@Override
+	public IStatus undo(IProgressMonitor monitor, IAdaptable info)
+			throws ExecutionException {
+		getEditor().removeRow(row.getKey());
+		getEditor().getEditorViewer().refresh();
+		return Status.OK_STATUS;
+	}
+
+}
