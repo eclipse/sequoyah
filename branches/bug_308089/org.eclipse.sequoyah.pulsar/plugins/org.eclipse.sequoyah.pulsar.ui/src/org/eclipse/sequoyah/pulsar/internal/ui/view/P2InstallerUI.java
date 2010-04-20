88ebc8a504f4c11478c6cf558c60b843862e3d39
dpastore <dpastore@7995dc58-227d-0410-baee-59875a487a5b>
2010-04-20 14:41:46 +0000

    Modifications to include Uninstall SDK dialog. https://bugs.eclipse.org/bugs/show_bug.cgi?id=308089
    git-svn-id: file:///svnroot/tools/org.eclipse.sequoyah@2059 7995dc58-227d-0410-baee-59875a487a5b

--- branches/bug_308089/org.eclipse.sequoyah.pulsar/plugins/org.eclipse.sequoyah.pulsar.ui/src/org/eclipse/sequoyah/pulsar/internal/ui/view/P2InstallerUI.java
+++ branches/bug_308089/org.eclipse.sequoyah.pulsar/plugins/org.eclipse.sequoyah.pulsar.ui/src/org/eclipse/sequoyah/pulsar/internal/ui/view/P2InstallerUI.java
@@ -23,20 +23,10 @@ import java.net.URI;
 import java.util.Collection;
 
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
-import org.eclipse.equinox.internal.p2.ui.IProvHelpContextIds;
-import org.eclipse.equinox.internal.p2.ui.dialogs.PreselectedIUInstallWizard;
-import org.eclipse.equinox.internal.p2.ui.dialogs.ProvisioningWizardDialog;
-import org.eclipse.equinox.internal.p2.ui.dialogs.UninstallWizard;
 import org.eclipse.equinox.p2.core.IProvisioningAgent;
 import org.eclipse.equinox.p2.core.ProvisionException;
-import org.eclipse.equinox.p2.engine.IProvisioningPlan;
 import org.eclipse.equinox.p2.metadata.IInstallableUnit;
 import org.eclipse.equinox.p2.operations.InstallOperation;
 import org.eclipse.equinox.p2.operations.ProvisioningSession;
@@ -50,20 +40,15 @@ import org.eclipse.equinox.p2.repository.metadata.IMetadataRepository;
 import org.eclipse.equinox.p2.repository.metadata.IMetadataRepositoryManager;
 import org.eclipse.equinox.p2.ui.LoadMetadataRepositoryJob;
 import org.eclipse.equinox.p2.ui.ProvisioningUI;
-import org.eclipse.jface.wizard.IWizard;
-import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.sequoyah.pulsar.internal.core.SDK;
 import org.eclipse.sequoyah.pulsar.internal.provisional.core.IInstallerUI;
 import org.eclipse.sequoyah.pulsar.internal.provisional.core.ISDK;
 import org.eclipse.sequoyah.pulsar.internal.provisional.core.ISDKRepository;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.PlatformUI;
 
 /**
  * A class implementing IInstallerUI with P2 install wizard
  */
-@SuppressWarnings("restriction")
 public class P2InstallerUI implements IInstallerUI {
 
     private static P2InstallerUI instance;
@@ -88,87 +73,66 @@ public class P2InstallerUI implements IInstallerUI {
         final ProvisioningUI defaultUI = ProvisioningUI.getDefaultUI();
 		ProvisioningSession session = defaultUI.getSession();
 		IProvisioningAgent agent = session.getProvisioningAgent();
+		
+		IMetadataRepositoryManager metadataManager = (IMetadataRepositoryManager) agent.getService(IMetadataRepositoryManager.SERVICE_NAME);
+		metadataManager.addRepository(metadataUri);
+		IArtifactRepositoryManager artifactManager = (IArtifactRepositoryManager) agent.getService(IArtifactRepositoryManager.SERVICE_NAME);
+		artifactManager.addRepository(artifactsUri);
+		LoadMetadataRepositoryJob loadMetadaJob = new LoadMetadataRepositoryJob(defaultUI);
+        final InstallOperation operation = new InstallOperation(session, ius);
+        
+        final int installWizard = defaultUI.openInstallWizard(ius, operation, loadMetadaJob);
+        
+//        Job job = new Job("Preparing") {
+//			protected IStatus run(IProgressMonitor monitor) {
+//				IStatus resolveStatus = operation.resolveModal(monitor);
+//				return resolveStatus;
+//			}
+//		};
+//		job.schedule();
+//		job.addJobChangeListener(new JobChangeAdapter() {
+//        	public void done(IJobChangeEvent event) {
+//        		final IProvisioningPlan plan = operation.getProvisioningPlan();
+//                if (plan != null) {
+//                    Display.getDefault().asyncExec(new Runnable() {
+//                        public void run() {
+//                        	LoadMetadataRepositoryJob loadMetadaJob = new LoadMetadataRepositoryJob(defaultUI);
+//                            IWizard wizard = new PreselectedIUInstallWizard(defaultUI, operation, ius, loadMetadaJob);
+//                            WizardDialog dialog = new WizardDialog(parentShell,
+//                                    wizard);
+//                            dialog.create();
+//                            PlatformUI.getWorkbench().getHelpSystem().setHelp(
+//                                    dialog.getShell(),
+//                                    IProvHelpContextIds.INSTALL_WIZARD);
+//                            dialog.open();
+//                        }
+//                    });
+//                }
+//        	}
+//        });
+        
+    }
+
+    public void runUninstaller(final Shell parentShell, ISDK sdk) throws CoreException {
+		SDK sdkImpl = (SDK) sdk.getAdapter(SDK.class);
+		final Collection<IInstallableUnit> ius = getInstallableUnits(sdkImpl);
+		final URI artifactsUri = sdkImpl.getRepository().getArtifactsURI();
+		final URI metadataUri = sdkImpl.getRepository().getMetadataURI();
+		
+		final ProvisioningUI defaultUI = ProvisioningUI.getDefaultUI();
+		ProvisioningSession session = defaultUI.getSession();
+		IProvisioningAgent agent = session.getProvisioningAgent();
+		final UninstallOperation operation = new UninstallOperation(session, ius);
+		LoadMetadataRepositoryJob loadMetadaJob = new LoadMetadataRepositoryJob(defaultUI);
+		
 		IMetadataRepositoryManager metadataManager = (IMetadataRepositoryManager) agent.getService(IMetadataRepositoryManager.SERVICE_NAME);
 		metadataManager.addRepository(metadataUri);
 		IArtifactRepositoryManager artifactManager = (IArtifactRepositoryManager) agent.getService(IArtifactRepositoryManager.SERVICE_NAME);
 		artifactManager.addRepository(artifactsUri);
 		
-        final InstallOperation operation = new InstallOperation(session, ius);
-        Job job = new Job("Preparing") {
-			protected IStatus run(IProgressMonitor monitor) {
-				IStatus resolveStatus = operation.resolveModal(monitor);
-				return resolveStatus;
-			}
-		};
-		job.schedule();
-		job.addJobChangeListener(new JobChangeAdapter() {
-        	public void done(IJobChangeEvent event) {
-        		final IProvisioningPlan plan = operation.getProvisioningPlan();
-                if (plan != null) {
-                    Display.getDefault().asyncExec(new Runnable() {
-                        public void run() {
-                        	LoadMetadataRepositoryJob loadMetadaJob = new LoadMetadataRepositoryJob(defaultUI);
-                            IWizard wizard = new PreselectedIUInstallWizard(defaultUI, operation, ius, loadMetadaJob);
-                            WizardDialog dialog = new WizardDialog(parentShell,
-                                    wizard);
-                            dialog.create();
-                            PlatformUI.getWorkbench().getHelpSystem().setHelp(
-                                    dialog.getShell(),
-                                    IProvHelpContextIds.INSTALL_WIZARD);
-                            dialog.open();
-                        }
-                    });
-                }
-        	}
-        });
-        
-    }
-
-    public void runUninstaller(final Shell parentShell, ISDK sdk)
-            throws CoreException {
-        SDK sdkImpl = (SDK) sdk.getAdapter(SDK.class);
-        final Collection<IInstallableUnit> ius = getInstallableUnits(sdkImpl);
-
-        final ProvisioningUI defaultUI = ProvisioningUI.getDefaultUI();
-		ProvisioningSession session = defaultUI.getSession();
-        final UninstallOperation operation = new UninstallOperation(session, ius);
-
-        Job job = new Job("Preparing") {
-			protected IStatus run(IProgressMonitor monitor) {
-				IStatus resolveStatus = operation.resolveModal(monitor);
-				return resolveStatus;
-			}
-		};
-		job.schedule();
-        
-		job.addJobChangeListener(new JobChangeAdapter() {
-			public void done(IJobChangeEvent event) {
-				final IProvisioningPlan plan = operation.getProvisioningPlan();
-				if (plan != null) {
-					if (PlatformUI.isWorkbenchRunning()) {
-						PlatformUI.getWorkbench().getDisplay().asyncExec(
-								new Runnable() {
-									public void run() {
-										LoadMetadataRepositoryJob loadMetadaJob = new LoadMetadataRepositoryJob(defaultUI);
-										UninstallWizard wizard = new UninstallWizard(
-												defaultUI, operation, ius, loadMetadaJob);
-										WizardDialog dialog = new ProvisioningWizardDialog(
-												parentShell, wizard);
-										dialog.create();
-										PlatformUI
-										.getWorkbench()
-										.getHelpSystem()
-										.setHelp(
-												dialog.getShell(),
-												IProvHelpContextIds.UNINSTALL_WIZARD);
-										dialog.open();
-									}
-								});
-					}
-				}
-			}
-		});
-    }
+		final int uninstallWizard = defaultUI.openUninstallWizard(ius, operation, loadMetadaJob);
+		
+	}
 
     public void refreshSDKRepositories(Collection<ISDKRepository> repositories) {
         /*
--- branches/bug_308089/org.eclipse.sequoyah.pulsar/plugins/org.eclipse.sequoyah.pulsar.ui/src/org/eclipse/sequoyah/pulsar/internal/ui/view/SDKInstallView.java
+++ branches/bug_308089/org.eclipse.sequoyah.pulsar/plugins/org.eclipse.sequoyah.pulsar.ui/src/org/eclipse/sequoyah/pulsar/internal/ui/view/SDKInstallView.java
@@ -18,25 +18,42 @@
  *  Henrique Magalhaes (Motorola) - Fixing update repository problem.
  *  Euclides Neto (Motorola) - Changed the method to refresh instead of remove SDK repositories.
  *  Euclides Neto (Motorola) - Adding SDK Category description support.
+ *  Daniel Franco (Eldorado) - [308089] Adding SDK Uninstall support.
+ *  Daniel Pastore (Eldorado) - [308089] Adding SDK Uninstall support.
  */
 
 package org.eclipse.sequoyah.pulsar.internal.ui.view;
 
 import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.equinox.internal.p2.core.DefaultAgentProvider;
 import org.eclipse.equinox.internal.p2.discovery.Catalog;
 import org.eclipse.equinox.internal.p2.discovery.DiscoveryCore;
 import org.eclipse.equinox.internal.p2.discovery.compatibility.BundleDiscoveryStrategy;
 import org.eclipse.equinox.internal.p2.discovery.compatibility.RemoteBundleDiscoveryStrategy;
+import org.eclipse.equinox.internal.p2.discovery.model.CatalogItem;
 import org.eclipse.equinox.internal.p2.ui.discovery.DiscoveryUi;
 import org.eclipse.equinox.internal.p2.ui.discovery.wizards.CatalogConfiguration;
 import org.eclipse.equinox.internal.p2.ui.discovery.wizards.CatalogViewer;
+import org.eclipse.equinox.p2.core.IProvisioningAgent;
+import org.eclipse.equinox.p2.engine.IProfile;
+import org.eclipse.equinox.p2.engine.IProfileRegistry;
+import org.eclipse.equinox.p2.metadata.IInstallableUnit;
+import org.eclipse.equinox.p2.operations.UninstallOperation;
+import org.eclipse.equinox.p2.query.IQuery;
+import org.eclipse.equinox.p2.query.IQueryResult;
+import org.eclipse.equinox.p2.query.QueryUtil;
+import org.eclipse.equinox.p2.ui.LoadMetadataRepositoryJob;
+import org.eclipse.equinox.p2.ui.ProvisioningUI;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
@@ -46,15 +63,18 @@ import org.eclipse.jface.preference.PreferenceDialog;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.sequoyah.pulsar.Activator;
+import org.eclipse.sequoyah.pulsar.internal.ui.UninstallDialog;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.BaseSelectionListenerAction;
 import org.eclipse.ui.dialogs.PreferencesUtil;
 import org.eclipse.ui.part.ViewPart;
+import org.osgi.framework.BundleContext;
 
 @SuppressWarnings("restriction")
 public class SDKInstallView extends ViewPart {
@@ -77,7 +97,30 @@ public class SDKInstallView extends ViewPart {
         	DiscoveryUi.install(viewer.getCheckedItems(), new ProgressMonitorDialog(getSite().getShell()));
         }
     }
+    
+	private static IProfile getProfile(String profileId)
+    {
+        IProfileRegistry profileRegistry = getProfileRegistry();
+        return profileRegistry.getProfile(profileId);
+    }
 
+    public static IProfileRegistry getProfileRegistry()
+    {
+        BundleContext context = Activator.getContext();
+        IProvisioningAgent agent = getProvisioningAgent(context);
+        IProfileRegistry profileRegistry =
+                (IProfileRegistry) agent.getService(IProfileRegistry.SERVICE_NAME);
+        return profileRegistry;
+    }
+	    
+    private static IProvisioningAgent getProvisioningAgent(BundleContext context)
+    {
+        DefaultAgentProvider agentProvider = new DefaultAgentProvider();
+        agentProvider.activate(context);
+        IProvisioningAgent agent = agentProvider.createAgent(null);
+        return agent;
+    }
+	    
     private class UninstallAction extends BaseSelectionListenerAction {
 
         protected UninstallAction() {
@@ -86,15 +129,42 @@ public class SDKInstallView extends ViewPart {
             setImageDescriptor(PlatformUI.getWorkbench().getSharedImages()
                     .getImageDescriptor(ISharedImages.IMG_ETOOL_DELETE));
         }
-
+        
         protected boolean updateSelection(IStructuredSelection selection) {
-        	// TODO return true if there are any installed sdks
-        	return false;
+        	return !getInstalledItems(viewer.getCatalog()).isEmpty();
         }
 
         @Override
         public void run() {
-        	// TODO implement using ListDialog??
+        	Set<CatalogItem> inst = getInstalledItems(viewer.getCatalog());
+        	if (inst.isEmpty()) {
+        		return;
+        	}
+        	
+        	Object[] installedItems = (inst.toArray());
+        	UninstallDialog uninstallDialog = new UninstallDialog(new Shell(), installedItems);
+        	int status = uninstallDialog.open();
+        	
+        	if (status == 0) {
+        		Object[] selectedItems = (uninstallDialog.getSelected().toArray());        		
+        		
+            	final ProvisioningUI defaultUI = ProvisioningUI.getDefaultUI();
+        		LoadMetadataRepositoryJob loadMetadaJob = new LoadMetadataRepositoryJob(defaultUI);
+        		IProfile profile = getProfile(defaultUI.getProfileId());
+        		
+        		HashSet<IQuery<IInstallableUnit>> queries = new HashSet<IQuery<IInstallableUnit>>();
+            	for (Object o : selectedItems) {
+            		CatalogItem selected = (CatalogItem) o;
+            		IQuery<IInstallableUnit> ius = QueryUtil.createIUQuery(((CatalogItem)selected).getId());
+            		queries.add(ius);            		
+            	}
+        		
+        		IQuery<IInstallableUnit> compoundedIUs = QueryUtil.createCompoundQuery(queries, false);        		
+        		IQueryResult<IInstallableUnit> iusCollection = profile.query(compoundedIUs, new NullProgressMonitor());
+        		final UninstallOperation operation = defaultUI.getUninstallOperation(iusCollection.toSet(), null);
+        		IStatus operationStatus = operation.resolveModal(new NullProgressMonitor());    		
+        		final int uninstallWizard = defaultUI.openUninstallWizard(iusCollection.toSet(), operation, loadMetadaJob);
+        	}
         }
     }
 
@@ -123,16 +193,14 @@ public class SDKInstallView extends ViewPart {
         }
 
         protected boolean updateSelection(IStructuredSelection selection) {
-        	// TODO return true if there are any installed sdks
-        	return false;
+        	return ((selection != null) && (!selection.isEmpty()));
         }
 
         @Override
         public void run() {
         	// TODO implement using ListDialog to select sdk and then get details?
 
-        	PreferenceDialog dialog = PreferencesUtil
-                    .createPreferenceDialogOn(
+        	PreferenceDialog dialog = PreferencesUtil.createPreferenceDialogOn(
                             viewer.getControl().getShell(),
                             "org.eclipse.sequoyah.ui.preferences.deviceManagementPreferencePage", //$NON-NLS-1$
                             new String[] { "org.eclipse.sequoyah.ui.preferences.deviceManagementPreferencePage" }, //$NON-NLS-1$
@@ -197,10 +265,30 @@ public class SDKInstallView extends ViewPart {
 		catalog.getDiscoveryStrategies().add(new BundleDiscoveryStrategy());
 
 		RemoteBundleDiscoveryStrategy remoteDiscoveryStrategy = new RemoteBundleDiscoveryStrategy();
-		remoteDiscoveryStrategy.setDirectoryUrl("http://download.eclipse.org/sequoyah/pulsar/discovery.xml");
+		//XML file with remote bundles directories
+		remoteDiscoveryStrategy.setDirectoryUrl("http://download.eclipse.org/sequoyah/pulsar/discovery.xml");//$NON-NLS-1$
 		catalog.getDiscoveryStrategies().add(remoteDiscoveryStrategy);
 
 		return catalog;
+	}
+	
+	/**
+	 * Returns a set with the Installed SDKs
+	 * 
+	 * @param catalog
+	 * @return Set<CatalogItem> selected
+	 */
+	private Set<CatalogItem> getInstalledItems(Catalog catalog)
+	{
+		Set<CatalogItem> selected = new HashSet<CatalogItem>();
+		for (Object descriptor : catalog.getItems().toArray()) {
+			if (descriptor instanceof CatalogItem) {
+				if (((CatalogItem) descriptor).isInstalled())
+					selected.add((CatalogItem) descriptor);
+			}
+		}
+
+		return selected;
 	}
 
 	/**
