d4010dc5da53d61d9f66cab391b2e4bf18160d23
dbarboza <dbarboza@7995dc58-227d-0410-baee-59875a487a5b>
2010-05-14 17:36:32 +0000

    JUnits for the Helios requirement
    git-svn-id: file:///svnroot/tools/org.eclipse.sequoyah@2108 7995dc58-227d-0410-baee-59875a487a5b

---
+++ trunk/org.eclipse.sequoyah.device/tests/src/org/eclipse/sequoyah/device/framework/junit/TestDevice.java
@@ -0,0 +1,51 @@
+/********************************************************************************
+ * Copyright (c) 2010 Motorola Inc.
+ * All rights reserved. This program and the accompanying materials are made available under the terms
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Initial Contributors:
+ * Daniel Barboza Franco (Eldorado)
+ * 
+ * Contributors:
+ * Contributor (Company) - Bug [NUMBER] - Bug Description
+ ********************************************************************************/
+
+package org.eclipse.sequoyah.device.framework.junit;
+
+import static org.junit.Assert.*;
+
+import java.util.Properties;
+
+import org.eclipse.sequoyah.device.common.utilities.exception.SequoyahException;
+import org.eclipse.sequoyah.device.framework.manager.InstanceManager;
+import org.eclipse.sequoyah.device.framework.model.IInstance;
+import org.junit.Test;
+
+public class TestDevice {
+
+	@Test
+	public void testCreateInstance() {
+		
+		String devName = "JUnit Device";
+		String devId = "org.eclipse.sequoyah.device.qemuarm.qemuarmDevice";
+		String status = "OFF";
+		Properties props = new Properties();
+		
+		IInstance instance = null;
+		
+		try {
+			instance = InstanceManager.createInstance(devName, devId, status, props);
+		} catch (SequoyahException e) {
+			fail(e.getMessage());
+			e.printStackTrace();
+		}
+		
+		assertNotNull(instance);
+		assertEquals(devName, instance.getName());
+		assertEquals(devId, instance.getDeviceTypeId());
+		assertEquals(status, instance.getStatus());
+		
+	}
+
+}
