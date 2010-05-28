156a25b92aedae85c9c61c2bde03332170b40f99
dpastore <dpastore@7995dc58-227d-0410-baee-59875a487a5b>
2010-05-28 17:21:19 +0000

    Applying patch from bug 314314 (https://bugs.eclipse.org/bugs/attachment.cgi?id=169863).
    git-svn-id: file:///svnroot/tools/org.eclipse.sequoyah@2142 7995dc58-227d-0410-baee-59875a487a5b

--- branches/android_stable/org.eclipse.sequoyah.android/plugins/org.eclipse.sequoyah.android.cdt.build.core/src/org/eclipse/sequoyah/android/cdt/build/core/INDKService.java
+++ branches/android_stable/org.eclipse.sequoyah.android/plugins/org.eclipse.sequoyah.android.cdt.build.core/src/org/eclipse/sequoyah/android/cdt/build/core/INDKService.java
@@ -4,17 +4,26 @@
 package org.eclipse.sequoyah.android.cdt.build.core;
 
 import org.eclipse.core.resources.IProject;
-
+import org.eclipse.core.runtime.QualifiedName;
+import org.eclipse.sequoyah.android.cdt.internal.build.core.Activator;
 
 /**
  * Service for getting information about the Android NDK.
  */
-public interface INDKService {
+public interface INDKService
+{
 
-	String getNDKLocation();
-	
-	void setNDKLocation(String location);
-	
-	void addNativeSupport(IProject project, String libraryName);
-	
+    // Property IDs
+
+    /**
+     * Library name property
+     */
+    public QualifiedName libName = new QualifiedName(Activator.PLUGIN_ID, "libName");
+
+    String getNDKLocation();
+
+    void setNDKLocation(String location);
+
+    void addNativeSupport(IProject project, String libraryName);
+
 }
--- branches/android_stable/org.eclipse.sequoyah.android/plugins/org.eclipse.sequoyah.android.cdt.build.ui/src/org/eclipse/sequoyah/android/cdt/internal/build/ui/AddNativeWizard.java
+++ branches/android_stable/org.eclipse.sequoyah.android/plugins/org.eclipse.sequoyah.android.cdt.build.ui/src/org/eclipse/sequoyah/android/cdt/internal/build/ui/AddNativeWizard.java
@@ -4,6 +4,7 @@
 package org.eclipse.sequoyah.android.cdt.internal.build.ui;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.sequoyah.android.cdt.build.core.INDKService;
 import org.eclipse.ui.IWorkbenchWindow;
@@ -13,52 +14,70 @@ import org.eclipse.ui.WorkbenchException;
  * @author dschaefer
  *
  */
-public class AddNativeWizard extends Wizard {
+public class AddNativeWizard extends Wizard
+{
 
-	private final IWorkbenchWindow window;
-	private final IProject project;
-	
-	private AddNativeProjectPage projectPage;
-	
-	public AddNativeWizard(IWorkbenchWindow window, IProject project) {
-		this.window = window;
-		this.project = project;
-		
-		setWindowTitle("Add Android Native Support");
-		setDialogSettings(Activator.getDefault().getDialogSettings());
-	}
-	
-	@Override
-	public void addPages() {
-		projectPage = new AddNativeProjectPage(project);
-		addPage(projectPage);
-	}
-	
-	@Override
-	public boolean canFinish() {
-		return projectPage.isNDKLocationValid();
-	}
-	
-	@Override
-	public boolean performFinish() {
-		// Switch to the C perspective
-		try {
-			window.getWorkbench().showPerspective("org.eclipse.cdt.ui.CPerspective", window); //$NON-NLS-1
-		} catch (WorkbenchException e) {
-			Activator.getDefault().getLog().log(e.getStatus());
-		}
+    private final IWorkbenchWindow window;
 
-		// Grab the data from the pages
-		final String libraryName = projectPage.getLibraryName();
+    private final IProject project;
 
-		// Save the NDK location
-		INDKService ndkService = Activator.getService(INDKService.class);
-		ndkService.setNDKLocation(projectPage.getNDKLocation());
-		
-		// Add the native support
-		ndkService.addNativeSupport(project, libraryName);
-		
-		return true;
-	}
+    private AddNativeProjectPage projectPage;
+
+    public AddNativeWizard(IWorkbenchWindow window, IProject project)
+    {
+        this.window = window;
+        this.project = project;
+
+        setWindowTitle("Add Android Native Support");
+        setDialogSettings(Activator.getDefault().getDialogSettings());
+    }
+
+    @Override
+    public void addPages()
+    {
+        projectPage = new AddNativeProjectPage(project);
+        addPage(projectPage);
+    }
+
+    @Override
+    public boolean canFinish()
+    {
+        return projectPage.isNDKLocationValid();
+    }
+
+    @Override
+    public boolean performFinish()
+    {
+        // Switch to the C perspective
+        try
+        {
+            window.getWorkbench().showPerspective("org.eclipse.cdt.ui.CPerspective", window); //$NON-NLS-1
+        }
+        catch (WorkbenchException e)
+        {
+            Activator.getDefault().getLog().log(e.getStatus());
+        }
+
+        // Grab the data from the pages
+        final String libraryName = projectPage.getLibraryName();
+        // Save the library name in the project properties.
+        try
+        {
+            project.setPersistentProperty(INDKService.libName, libraryName);
+        }
+        catch (CoreException e)
+        {
+            Activator.getDefault().getLog().log(e.getStatus());
+        }
+
+        // Save the NDK location
+        INDKService ndkService = Activator.getService(INDKService.class);
+        ndkService.setNDKLocation(projectPage.getNDKLocation());
+
+        // Add the native support
+        ndkService.addNativeSupport(project, libraryName);
+
+        return true;
+    }
 
 }
