ae8c2747ea22ec9f7fbeea909d9cb9c1cd2352ca
dpastore <dpastore@7995dc58-227d-0410-baee-59875a487a5b>
2010-05-31 20:27:22 +0000

    Applying patch from bug 315122 (https://bugs.eclipse.org/bugs/attachment.cgi?id=170581).
    git-svn-id: file:///svnroot/tools/org.eclipse.sequoyah@2145 7995dc58-227d-0410-baee-59875a487a5b

--- branches/android_stable/org.eclipse.sequoyah.android/plugins/org.eclipse.sequoyah.android.cdt.build.ui/src/org/eclipse/sequoyah/android/cdt/internal/build/ui/AddNativeProjectPage.java
+++ branches/android_stable/org.eclipse.sequoyah.android/plugins/org.eclipse.sequoyah.android.cdt.build.ui/src/org/eclipse/sequoyah/android/cdt/internal/build/ui/AddNativeProjectPage.java
@@ -6,20 +6,24 @@ package org.eclipse.sequoyah.android.cdt.internal.build.ui;
 import java.io.File;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.QualifiedName;
+import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.jface.preference.PreferenceDialog;
+import org.eclipse.jface.preference.PreferenceManager;
 import org.eclipse.sequoyah.android.cdt.build.core.INDKService;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Link;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.WorkbenchException;
 
 /**
  * @author dschaefer
@@ -27,17 +31,32 @@ import org.eclipse.swt.widgets.Text;
  */
 public class AddNativeProjectPage extends WizardPage {
 
-	private final IProject project;
+	@Override
+	public boolean canFlipToNextPage() {
+		
+		return false;
+	}
+	
+	private String libName = ""; //$NON-NLS-1$
+	
+	private Link setNDKLocationLink;
 	
 	private Text location;
-	private Text libraryName;
+	private Text libraryText;
+	boolean isNewProjectWizardPage = false;
 	
-	public AddNativeProjectPage(IProject project) {
-		super("projectPage");
-		setTitle("Project");
-		setDescription("Settings for adding native support to the project");
-		
-		this.project = project;
+	public AddNativeProjectPage(String libraryName, boolean isNewProjectWizardPage) {
+		this(isNewProjectWizardPage);
+		this.libName = libraryName;
+	}
+	
+	public AddNativeProjectPage(boolean isNewProjectWizardPage) {
+		super("native_page"); //$NON-NLS-1$
+		setTitle(Messages.AddNativeProjectPage_native_page_title);
+		setDescription(Messages.AddNativeProjectPage_native_page_description);
+ 		
+
+		this.isNewProjectWizardPage = isNewProjectWizardPage;
 	}
 
 	public void createControl(Composite parent) {
@@ -55,56 +74,66 @@ public class AddNativeProjectPage extends WizardPage {
 	private void addNDKLocation(Composite parent) {
 		Group group = new Group(parent, SWT.NONE);
 		GridLayout layout = new GridLayout();
-		layout.numColumns = 2;
+		layout.numColumns = 1;
 		group.setLayout(layout);
 		group.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
-		group.setText("NDK Location");
+		group.setText(Messages.AddNativeProjectPage_ndk_location_group_text);
 
 		location = new Text(group, SWT.BORDER);
 		location.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		location.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
-				validateNDKLocation();
-			}
-		});
-		validateNDKLocation();
+		location.setEditable(false);
 		
-		String ndkLocDir = UIPlugin.getService(INDKService.class).getNDKLocation();
-		if (ndkLocDir != null)
-			location.setText(ndkLocDir);
+		if(!isNDKLocationValid())
+		{
+			setErrorMessage(Messages.AddNativeProjectPage_ndk_invalid_path_msg);
+		}
 		
-		Button browse = new Button(group, SWT.NONE);
-		browse.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, true));
-		browse.setText("Browse");
-		browse.addSelectionListener(new SelectionListener() {
-			public void widgetSelected(SelectionEvent e) {
-				DirectoryDialog dialog = new DirectoryDialog(location.getShell());
-				dialog.setMessage("NDK Location");
-				String dir = dialog.open();
-				if (dir != null)
-					location.setText(dir);
+		String path = PlatformUI.getPreferenceStore().getString(UIPlugin.NDK_LOCATION_PREFERENCE);
+		if(path != null)
+		{
+			location.setText(path);
+		}		setNDKLocationLink = new Link(group, SWT.NONE);
+		GridData gridData = new GridData(SWT.RIGHT, SWT.CENTER, true, false, 1, 1);
+        setNDKLocationLink.setLayoutData(gridData);
+        setNDKLocationLink.setText(Messages.AddNativeProjectPage_ndk_preference_link_text);
+        setNDKLocationLink.setEnabled(true);
+        setNDKLocationLink.addListener(SWT.Selection, new Listener()
+        {
+			public void handleEvent(Event event) {
+				PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable()
+                {
+                    public void run()
+                    {
+                    	PreferenceManager prefManager = PlatformUI.getWorkbench().getPreferenceManager();
+                        PreferenceDialog dialog = new PreferenceDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+                                        .getShell(), prefManager);
+                        dialog.setSelectedNode("org.eclipse.sequoyah.android.cdt.internal.build.ui.preferencepage"); //$NON-NLS-1$
+                        
+                        dialog.create();
+                        dialog.setBlockOnOpen(true);
+                        dialog.open();
+                        //blocks until user clicks ok
+                        String path = PlatformUI.getPreferenceStore().getString(UIPlugin.NDK_LOCATION_PREFERENCE);
+                        location.setText(path);
+                        
+                        if(!isNDKLocationValid())
+                		{
+                        	setErrorMessage(Messages.AddNativeProjectPage_ndk_invalid_path_msg); //$NON-NLS-1$
+                		}
+                        else if(!isLibraryNameValid())
+                        {
+                        	setErrorMessage(Messages.AddNativeProjectPage_empty_library_name_msg);
+                        }
+                        else
+                        {
+                        	setErrorMessage(null);
+                        }
+                        getWizard().getContainer().updateButtons();
+                    }
+                });
 			}
-			
-			public void widgetDefaultSelected(SelectionEvent e) {
-			}
-		});
-	}
-
-	private void validateNDKLocation() {
-		if (isNDKLocationValid())
-			setErrorMessage(null);
-		else
-			setErrorMessage("Invalid Android NDK location");
-		
-		if (getWizard().getContainer().getCurrentPage() != null)
-			getWizard().getContainer().updateButtons();
-	}
-	
-	public boolean isNDKLocationValid() {
-		String locStr = location.getText();
-		File locFile = new File(locStr);
-		return locFile.exists();
-	}
+        });
+ 	}
 	
 	public String getNDKLocation() {
 		return location.getText();
@@ -115,15 +144,116 @@ public class AddNativeProjectPage extends WizardPage {
 		GridLayout layout = new GridLayout();
 		group.setLayout(layout);
 		group.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
-		group.setText("Library name (lib*.so will be added)");
+		group.setText(Messages.AddNativeProjectPage_library_name_group_text);
 		
-		libraryName = new Text(group, SWT.BORDER);
-		libraryName.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		libraryName.setText(project.getName());
+		libraryText = new Text(group, SWT.BORDER);
+		libraryText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		libraryText.setText(libName);
+		
+		if(!isLibraryNameValid())
+		{
+			setErrorMessage(Messages.AddNativeProjectPage_empty_library_name_msg); //$NON-NLS-1$
+		}
+		libraryText.addListener(SWT.Modify, new Listener()
+		{
+			public void handleEvent(Event event) 
+			{
+				if(!isLibraryNameValid())
+                {
+                	setErrorMessage(Messages.AddNativeProjectPage_empty_library_name_msg); //$NON-NLS-1$
+                }
+				else if(!isNDKLocationValid())
+        		{
+                	setErrorMessage(Messages.AddNativeProjectPage_ndk_invalid_path_msg); //$NON-NLS-1$
+        		}
+                else 
+                {
+                	setErrorMessage(null);
+                }
+				getWizard().getContainer().updateButtons();
+			}
+		});
 	}
 
 	public String getLibraryName() {
-		return libraryName.getText();
+		return libraryText.getText();
+	}
+
+	//returns null. There is no next page
+	@Override
+	public IWizardPage getNextPage() 
+	{
+		return null;
+	}
+
+	//true when valid ndk path is set and library name specified
+	@Override
+	public boolean isPageComplete() {
+		boolean isComplete = false;
+		
+		if(isNDKLocationValid() && isLibraryNameValid())
+		{
+			isComplete = true;
+		}
+		
+		return isComplete;
+	}
+	
+	public boolean isNDKLocationValid()
+	{
+		boolean isValid = true;
+		String path = PlatformUI.getPreferenceStore().getString(UIPlugin.NDK_LOCATION_PREFERENCE);
+		
+		if(path.length() == 0 || !NDKPreferencePage.validateNDKDirectory(path))
+		{
+			isValid = false;
+		}
+		return isValid;
+	}
+	
+	public boolean isLibraryNameValid()
+	{
+		boolean isValid = false;
+		String libName = libraryText.getText();
+		if(libName.trim().length() > 0)
+		{
+			isValid = true;
+		}
+		return isValid;
+	}
+	
+	public void initializeLibraryField(String libName)
+	{
+		libraryText.setText(libName);
+		libraryText.update();
+	}
+	
+	public boolean performFinish(IWorkbenchWindow window, IProject project)
+	{
+		boolean success = true;
+		try
+		{
+			// Switch to the C perspective
+			window.getWorkbench().showPerspective("org.eclipse.cdt.ui.CPerspective", window); //$NON-NLS-1 //$NON-NLS-1$
+
+			// Grab the data from the pages
+			final String libraryName = getLibraryName();
+
+			// Save the NDK location
+			INDKService ndkService = Activator.getService(INDKService.class);
+			ndkService.setNDKLocation(getNDKLocation());
+
+			// Add the native support
+			ndkService.addNativeSupport(project, libraryName);
+			
+    		project.setPersistentProperty(UIPlugin.libName, libraryName);
+		} 
+		catch (WorkbenchException e) 
+		{
+			Activator.getDefault().getLog().log(e.getStatus());
+			success = false;
+		}
+		return success;
 	}
 	
 }
--- branches/android_stable/org.eclipse.sequoyah.android/plugins/org.eclipse.sequoyah.android.cdt.build.ui/src/org/eclipse/sequoyah/android/cdt/internal/build/ui/AddNativeWizard.java
+++ branches/android_stable/org.eclipse.sequoyah.android/plugins/org.eclipse.sequoyah.android.cdt.build.ui/src/org/eclipse/sequoyah/android/cdt/internal/build/ui/AddNativeWizard.java
@@ -4,7 +4,6 @@
 package org.eclipse.sequoyah.android.cdt.internal.build.ui;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.sequoyah.android.cdt.build.core.INDKService;
 import org.eclipse.ui.IWorkbenchWindow;
@@ -14,70 +13,35 @@ import org.eclipse.ui.WorkbenchException;
  * @author dschaefer
  *
  */
-public class AddNativeWizard extends Wizard
-{
-
-    private final IWorkbenchWindow window;
-
-    private final IProject project;
-
-    private AddNativeProjectPage projectPage;
-
-    public AddNativeWizard(IWorkbenchWindow window, IProject project)
-    {
-        this.window = window;
-        this.project = project;
-
-        setWindowTitle("Add Android Native Support");
-        setDialogSettings(UIPlugin.getDefault().getDialogSettings());
-    }
-
-    @Override
-    public void addPages()
-    {
-        projectPage = new AddNativeProjectPage(project);
-        addPage(projectPage);
-    }
-
-    @Override
-    public boolean canFinish()
-    {
-        return projectPage.isNDKLocationValid();
-    }
-
-    @Override
-    public boolean performFinish()
-    {
-        // Switch to the C perspective
-        try
-        {
-            window.getWorkbench().showPerspective("org.eclipse.cdt.ui.CPerspective", window); //$NON-NLS-1
-        }
-        catch (WorkbenchException e)
-        {
-            UIPlugin.getDefault().getLog().log(e.getStatus());
-        }
-
-        // Grab the data from the pages
-        final String libraryName = projectPage.getLibraryName();
-        // Save the library name in the project properties.
-        try
-        {
-            project.setPersistentProperty(INDKService.libName, libraryName);
-        }
-        catch (CoreException e)
-        {
-            UIPlugin.getDefault().getLog().log(e.getStatus());
-        }
-
-        // Save the NDK location
-        INDKService ndkService = UIPlugin.getService(INDKService.class);
-        ndkService.setNDKLocation(projectPage.getNDKLocation());
-
-        // Add the native support
-        ndkService.addNativeSupport(project, libraryName);
-
-        return true;
-    }
+public class AddNativeWizard extends Wizard {
+	private final IWorkbenchWindow window;
+	private final IProject project; 
+	
+	private AddNativeProjectPage projectPage;
+	
+	public AddNativeWizard(IWorkbenchWindow window, IProject project) {
+		this.window = window;
+		this.project = project;
+		
+		setWindowTitle(Messages.AddNativeWizard_native_wizard_title);
+		setDialogSettings(Activator.getDefault().getDialogSettings());
+	}
+	
+	@Override
+	public void addPages() {
+		projectPage = new AddNativeProjectPage(project.getName(), false);
+		addPage(projectPage);
+	}
+	
+	@Override
+	public boolean canFinish() {
+		return projectPage.isNDKLocationValid() && projectPage.isLibraryNameValid();
+	}
+	
+	@Override
+	public boolean performFinish() {
+				
+		return projectPage.performFinish(window, project);
+	}
 
 }
