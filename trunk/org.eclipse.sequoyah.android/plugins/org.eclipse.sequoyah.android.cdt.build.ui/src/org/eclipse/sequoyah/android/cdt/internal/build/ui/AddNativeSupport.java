154770ff279b02b14ad0dcbc9761a3dda553ba90
dpastore <dpastore@7995dc58-227d-0410-baee-59875a487a5b>
2010-07-03 18:43:05 +0000

    Applying patch 173319 (https://bugs.eclipse.org/bugs/attachment.cgi?id=173319) from bug 318712 (https://bugs.eclipse.org/bugs/show_bug.cgi?id=318712).
    git-svn-id: file:///svnroot/tools/org.eclipse.sequoyah@2192 7995dc58-227d-0410-baee-59875a487a5b

--- trunk/org.eclipse.sequoyah.android/plugins/org.eclipse.sequoyah.android.cdt.build.ui/src/org/eclipse/sequoyah/android/cdt/internal/build/ui/AddNativeSupport.java
+++ trunk/org.eclipse.sequoyah.android/plugins/org.eclipse.sequoyah.android.cdt.build.ui/src/org/eclipse/sequoyah/android/cdt/internal/build/ui/AddNativeSupport.java
@@ -1,5 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2010 Wind River Systems and others.
+ * Copyright (c) 2010 Motorola, Inc. All rights reserved.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,11 +9,11 @@
  * Contributors:
  * Doug Schaefer (WRS) - Initial API and implementation
  * Carlos Alberto Souto Junior - Further improvements in the Wizard
+ * Marcelo Marzola Bossoni - [318712] Make run wizard an static method and make the add native support calls this method
  *******************************************************************************/
 package org.eclipse.sequoyah.android.cdt.internal.build.ui;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.PlatformObject;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
@@ -24,58 +25,56 @@ import org.eclipse.ui.IWorkbenchPart;
 /**
  * 
  */
-public class AddNativeSupport implements IObjectActionDelegate
-{
+public class AddNativeSupport implements IObjectActionDelegate {
 
-    private IWorkbenchPart targetPart;
+	private IWorkbenchPart targetPart;
 
-    private IProject project;
+	private IProject project;
 
-    public void run(IAction action)
-    {
-        // check windows project location restrictions (cygwin does not work for project with
-        // whitespaces on its path)
-        if (Platform.getOS().equals(Platform.OS_WIN32)
-                && project.getLocation().toOSString().contains(" "))
-        {
-            MessageUtils.showErrorDialog(
-                    Messages.AddNativeProjectAction_InvalidProjectLocation_Title, Messages.bind(
-                            Messages.AddNativeProjectAction_InvalidProjectLocation_Message, project
-                                    .getLocation().toOSString()));
-        }
-        else
-        {
-            // Run the wizard
-            AddNativeWizard wizard =
-                    new AddNativeWizard(targetPart.getSite().getWorkbenchWindow(), project);
-            WizardDialog dialog = new WizardDialog(targetPart.getSite().getShell(), wizard);
-            dialog.open();
-        }
-    }
+	public void run(IAction action) {
+		runWizard(project, targetPart);
+	}
 
-    public void selectionChanged(IAction action, ISelection selection)
-    {
-        if (selection instanceof IStructuredSelection)
-        {
-            Object selected = ((IStructuredSelection) selection).getFirstElement();
-            if (selected instanceof IProject)
-            {
-                project = (IProject) selected;
-            }
-            else if (selected instanceof PlatformObject)
-            {
-                project = (IProject) ((PlatformObject) selected).getAdapter(IProject.class);
-            }
-            else
-            {
-                project = null;
-            }
-        }
-    }
+	public static void runWizard(IProject project, IWorkbenchPart part) {
+		// check windows project location restrictions (cygwin does not work for
+		// project with
+		// whitespaces on its path)
+		if ((project != null)
+				&& project.getLocation().toOSString().contains(" ")) {
+			MessageUtils
+					.showErrorDialog(
+							Messages.AddNativeProjectAction_InvalidProjectLocation_Title,
+							Messages.bind(
+									Messages.AddNativeProjectAction_InvalidProjectLocation_Message,
+									project.getLocation().toOSString()));
+		} else {
+			// Run the wizard
+			AddNativeWizard wizard = new AddNativeWizard(part.getSite()
+					.getWorkbenchWindow(), project);
+			WizardDialog dialog = new WizardDialog(part.getSite().getShell(),
+					wizard);
+			dialog.open();
+		}
 
-    public void setActivePart(IAction action, IWorkbenchPart targetPart)
-    {
-        this.targetPart = targetPart;
-    }
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+		if (selection instanceof IStructuredSelection) {
+			Object selected = ((IStructuredSelection) selection)
+					.getFirstElement();
+			if (selected instanceof IProject) {
+				project = (IProject) selected;
+			} else if (selected instanceof PlatformObject) {
+				project = (IProject) ((PlatformObject) selected)
+						.getAdapter(IProject.class);
+			} else {
+				project = null;
+			}
+		}
+	}
+
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		this.targetPart = targetPart;
+	}
 
 }
