9ce8922c59ea2e03ccfb4c2efbacb54495d871ce
dpastore <dpastore@7995dc58-227d-0410-baee-59875a487a5b>
2010-08-06 14:06:54 +0000

    Tag for Helios build (3.6_SR1_RC1+0)
    git-svn-id: file:///svnroot/tools/org.eclipse.sequoyah@2231 7995dc58-227d-0410-baee-59875a487a5b

---
+++ tags/3.6_SR1_RC1+0/org.eclipse.sequoyah.vnc.vncviewer/src/org/eclipse/sequoyah/vnc/vncviewer/network/RectHeader.java
@@ -0,0 +1,47 @@
+/********************************************************************************
+ * Copyright (c) 2008-2010 MontaVista Software. All rights reserved.
+ * This program and the accompanying materials are made available under the terms
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Initial Contributor:
+ * Eugene Melekhov (Montavista) - Bug [227793] - Implementation of the several encodings, performance enhancement etc
+ *
+ * Contributors:
+ * Daniel Pastore (Eldorado) - [289870] Moving and renaming Tml to Sequoyah
+ ********************************************************************************/
+package org.eclipse.sequoyah.vnc.vncviewer.network;
+
+public class RectHeader {
+
+	protected int x;
+	protected int y;
+	protected int width;
+	protected int height;
+	protected int encoding;
+
+	public RectHeader(int x, int y, int width, int height, int encoding) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.encoding = encoding;
+	}
+
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public int getEncoding() {
+		return encoding;
+	}
+	
+}
