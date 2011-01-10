8e45ec30bad223b5ef5de710991f4aadedfb21c7
dpastore <dpastore@7995dc58-227d-0410-baee-59875a487a5b>
2011-01-10 12:59:19 +0000

    Copyright fixes.
    git-svn-id: file:///svnroot/tools/org.eclipse.sequoyah@2350 7995dc58-227d-0410-baee-59875a487a5b

--- trunk/org.eclipse.sequoyah.localization/plugins/org.eclipse.sequoyah.localization.stringeditor/src/org/eclipse/sequoyah/localization/editor/datatype/RowInfoLeaf.java
+++ trunk/org.eclipse.sequoyah.localization/plugins/org.eclipse.sequoyah.localization.stringeditor/src/org/eclipse/sequoyah/localization/editor/datatype/RowInfoLeaf.java
@@ -1,3 +1,16 @@
+/********************************************************************************
+ * Copyright (c) 2010 Motorola Mobility, Inc.
+ * All rights reserved. This program and the accompanying materials are made available under the terms
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Initial Contributors:
+ * Julia Martinez Perdigueiro (Eldorado)
+ * 
+ * Contributors:
+ * Name (Company) - [Bug] - Description
+ ********************************************************************************/
+
 package org.eclipse.sequoyah.localization.editor.datatype;
 
 import java.util.HashMap;
