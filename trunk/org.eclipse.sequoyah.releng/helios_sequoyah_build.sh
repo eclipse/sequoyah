3845e3debd9e18d701615b9737b443538f3fc5d8
dpastore <dpastore@7995dc58-227d-0410-baee-59875a487a5b>
2011-03-03 17:55:56 +0000

    Updating releng package.
    git-svn-id: file:///svnroot/tools/org.eclipse.sequoyah@2367 7995dc58-227d-0410-baee-59875a487a5b

--- trunk/org.eclipse.sequoyah.releng/helios_sequoyah_build.sh
+++ trunk/org.eclipse.sequoyah.releng/helios_sequoyah_build.sh
@@ -1,8 +1,8 @@
-export JAVA_HOME=/opt/public/dsdp/JDKs/ibm-java2-ppc-50
+export JAVA_HOME=/opt/public/common/jdk-1.5.0-22.x86_64
 export PATH=$JAVA_HOME/bin:$PATH
 export BUILD_BASE_PATH=/opt/public/dsdp/sequoyah/helios
 cd /opt/public/dsdp/sequoyah/helios
 rm -rf src
-cd build/sequoyah
+cd build/sequoyah/branches/3.6_SR
 ant -buildfile build.xml
 
