1b98215fa4e3ac47b163f2ba46037dbddaa541bd
dpastore <dpastore@7995dc58-227d-0410-baee-59875a487a5b>
2011-03-10 16:48:14 +0000

    Garbage collection (removing old branches).
    git-svn-id: file:///svnroot/tools/org.eclipse.sequoyah@2377 7995dc58-227d-0410-baee-59875a487a5b

--- branches/build_rev2134/org.eclipse.sequoyah.releng/helios/build/build.sh
+++
@@ -1,8 +0,0 @@
-export JAVA_HOME=/opt/public/dsdp/JDKs/ibm-java2-ppc-50
-export PATH=$JAVA_HOME/bin:$PATH
-export BUILD_BASE_PATH=/opt/public/dsdp/sequoyah/helios
-cd ..
-rm -rf src
-cd build
-ant -buildfile build.xml
-
