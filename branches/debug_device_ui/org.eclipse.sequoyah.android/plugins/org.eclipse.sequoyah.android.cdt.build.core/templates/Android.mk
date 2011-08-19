2f2c5ec0f3bb9bb8f7f3c3b53e5432b9cf4d8df7
mgorri <mgorri@7995dc58-227d-0410-baee-59875a487a5b>
2011-08-19 12:55:18 +0000

    removing debug branch for PB investigation.
    git-svn-id: file:///svnroot/tools/org.eclipse.sequoyah@2445 7995dc58-227d-0410-baee-59875a487a5b

--- branches/debug_device_ui/org.eclipse.sequoyah.android/plugins/org.eclipse.sequoyah.android.cdt.build.core/templates/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := %{lib}
-### Add all source file names to be included in lib separated by a whitespace
-LOCAL_SRC_FILES :=
-
-include $(BUILD_SHARED_LIBRARY)
